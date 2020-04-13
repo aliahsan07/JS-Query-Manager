b297bf6ccc4423c3260ef5e42ea34d658389e5ac
Ali Ahsan <aliahsan07@outlook.com>
2020-04-13 17:46:59 -0500

    more test cases added, specifically for ctx and obj sensitivity

---
+++ test-suite/object-sensitivity/object-sensitivity-1.js
@@ -0,0 +1,24 @@
+function X(num) {
+  this.num = num;
+}
+
+function Y() {
+  this.f = null;
+  this.setF = function (x) {
+    this.f = x;
+  };
+}
+
+var x1 = new X("1");
+var x2 = new X("2");
+var y1 = new Y();
+var y2 = new Y();
+y1.setF(x1);
+y2.setF(x2);
+
+// y1.f should only point to x1
+// y2.f should only point to x2
+y1.f;
+y2.f;
+
+var groundTruth = { "f-21": 1, "f-22": 1 };
