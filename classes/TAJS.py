bea30d10da94e94bd9cfc7a6375d0baa0ac75a7c
Ali Ahsan <aliahsan07@outlook.com>
2020-04-20 02:59:39 -0500

    Added flags to TAJS class for more options to run analysis

--- classes/TAJS.py
+++ classes/TAJS.py
@@ -1,16 +1,43 @@
 from subprocess import Popen, PIPE, STDOUT
 from classes.Analysis import Analysis
 from utils.readTool import readToolOutput
+import itertools
+import os
 
 
 class TAJS(Analysis):
 
     def __init__(self, t=[]):
         super().__init__(t)
+        self.baseCommand = ['java', '-jar', '../TAJS/TAJS-run/dist/tajs-all.jar',
+                            '-ptrSetFile', self.outputFile]
+        self.flags = ["-uneval", "-determinacy",
+                      ("-blended-analysis", "logFile"), ("-unsound", "X")]
+        self.combinations = []
+        for L in range(0, len(self.flags)+1):
+            for subset in itertools.combinations(self.flags, L):
+                self.combinations.append(subset)
 
-    def run(self):
+    def runAllCombinations(self):
+        for combination in self.combinations:
+            self.run(*combination)
+
+    def run(self, *flags):
         print(">>>>> Running TAJS on JS Program <<<<< ")
-        tajsOutput = Popen(['java', '-jar', '../TAJS/TAJS-run/dist/tajs-all.jar',
-                            '-ptrSetFile', self.outputFile, self.analysisFile], stdout=PIPE, stderr=STDOUT)
+        command = self.baseCommand.copy()
+        for arg in flags:
+            if isinstance(arg, tuple):
+                if arg[0] == '-blended-analysis':
+                    f = arg[1]
+                    if not os.path.isfile(f):
+                        print("Error: log file not found for blended analysis")
+                        continue
+                command.append(arg[0])
+                command.append(arg[1])
+            else:
+                command.append(arg)
 
+        command.append(self.analysisFile)
+        tajsOutput = Popen(command, stdout=PIPE, stderr=STDOUT)
+        print(command)
         return readToolOutput(tajs=True)
