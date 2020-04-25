8cd21d9911d52f8ec39830ad2481b01f9b808f4c
Ali Ahsan <aliahsan07@outlook.com>
2020-04-24 19:15:21 -0500

    minor updates

--- classes/Safe.py
+++ classes/Safe.py
@@ -5,17 +5,41 @@ from utils.readTool import readToolOutput
 
 class Safe(Analysis):
 
-    def __init__(self, t=[]):
+    def __init__(self, t=[], loopDepth=None, loopIter=None, callsiteSensitivity=None):
         super().__init__(t)
         self.baseCommand = ['safe', 'analyze',
                             '-analyzer:ptrSetFile=' + self.outputFile]
+        self.loopDepth = loopDepth
+        self.loopIter = loopIter
+        self.callsiteSensitivity = callsiteSensitivity
 
     def runWithRecencyAbstraction(self):
         return self.run('-heapBuilder:recency')
 
+    def setCallsiteSensitivity(self, n):
+        self.callsiteSensitivity = n
+
+    def setLoopIter(self, n):
+        self.loopIter = n
+
+    def setloopDepth(self, n):
+        self.loopDepth = n
+
+    def appendHeapBuilderFlags(self, command):
+        if self.callsiteSensitivity is not None:
+            command.append('-heapBuilder:callsiteSensitivity=' +
+                           str(self.callsiteSensitivity))
+
+        if self.loopDepth is not None:
+            command.append('-heapBuilder:loopDepth=' + str(self.loopDepth))
+
+        if self.loopIter is not None:
+            command.append('-heapBuilder:loopIter=' + str(self.loopIter))
+
     def run(self, *flags):
         print(">>>>> Running Safe on JS Program <<<<< ")
         command = self.baseCommand.copy()
+        self.appendHeapBuilderFlags(command)
         for arg in flags:
             command.append(arg)
         command.append(self.analysisFile)
